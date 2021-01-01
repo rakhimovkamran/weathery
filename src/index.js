301ccafdc90e524ec6f954f40406d01077ab04d8
rakhimovkamran <rakhimovkamran@gmail.com>
2021-01-01 14:17:38 +0500

    Update Entry point

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { MainView } from "./views/Main";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <MainView />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
